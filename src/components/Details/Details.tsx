df7c91c3f698c267fa05c6be1d35c82ea90d9cf4
iampooran <pooransen50@gmail.com>
2023-04-16 23:23:39 +0530

    Success Message Added

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -1,12 +1,34 @@
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet, Image} from 'react-native';
 import React from 'react';
 
 const Details = () => {
   return (
-    <View>
-      <Text>Details Page</Text>
+    <View style={styles.container}>
+      <Text style={styles.loginMessage}>
+        You have Successfully Login to App!
+      </Text>
+      <Image
+        source={{
+          width: 200,
+          height: 200,
+          uri: 'https://icons.veryicon.com/png/o/miscellaneous/8atour/submit-successfully.png',
+        }}
+      />
     </View>
   );
 };
 
 export default Details;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  loginMessage: {
+    color: 'green',
+    marginBottom: 10,
+  },
+});
