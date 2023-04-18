4ab9e417bd02e9d7197d4307d747638709a5d2d1
iampooran <pooransen50@gmail.com>
2023-04-18 14:08:25 +0530

    Changes

--- src/components/NavigationOptions/NavigationOptions.tsx
+++ src/components/NavigationOptions/NavigationOptions.tsx
@@ -3,12 +3,20 @@ import React from 'react';
 
 const NavigationOptions = ({navigation}: {navigation: any}) => {
   const handleBottomTab = () => {
+    navigation.navigate('Authenticated');
+  };
+
+  const handleMaterialBottomTab = () => {
     navigation.navigate('MaterialBottomTab');
   };
 
   return (
     <View style={styles.container}>
       <Button title="Go To Bottom Tab" onPress={() => handleBottomTab()} />
+      <Button
+        title="Go To Material Bottom Tab"
+        onPress={() => handleMaterialBottomTab()}
+      />
     </View>
   );
 };
