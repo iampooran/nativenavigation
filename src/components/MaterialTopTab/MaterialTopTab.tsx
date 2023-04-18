aa08a70facb589822ac881f0ca976dddff3f0ad0
iampooran <pooransen50@gmail.com>
2023-04-18 14:26:18 +0530

    BottomTab Added

--- src/components/MaterialTopTab/MaterialTopTab.tsx
+++ src/components/MaterialTopTab/MaterialTopTab.tsx
@@ -1,11 +1,18 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import Details from '../Details/Details';
+import Order from '../Order/Order';
+import Settings from '../Settings/Setting';
+
+const Tab = createMaterialTopTabNavigator();
 
 const MaterialTopTab = () => {
   return (
-    <View>
-      <Text>MaterialTopTab</Text>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Details" component={Details} />
+      <Tab.Screen name="Order" component={Order} />
+      <Tab.Screen name="Settings" component={Settings} />
+    </Tab.Navigator>
   );
 };
 
