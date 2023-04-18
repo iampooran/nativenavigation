e0c76b5f56561bc54dbb16057d7e32771a090b14
iampooran <pooransen50@gmail.com>
2023-04-18 14:15:00 +0530

    MaterialBottomTabAdded

--- src/components/MaterialBottomTab/MaterialBottomTab.tsx
+++ src/components/MaterialBottomTab/MaterialBottomTab.tsx
@@ -1,11 +1,47 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import Details from '../Details/Details';
+import Settings from '../Settings/Setting';
+import Order from '../Order/Order';
+import Icon from 'react-native-vector-icons/EvilIcons';
+
+const Tab = createMaterialBottomTabNavigator();
+
+const MyIcon = () => {
+  return <Icon name="star" size={30} />;
+};
 
 const MaterialBottomTab = () => {
   return (
-    <View>
-      <Text>MaterialBottomTab</Text>
-    </View>
+    <Tab.Navigator
+      initialRouteName="Feed"
+      activeColor="black"
+      barStyle={{backgroundColor: 'skyblue'}}>
+      <Tab.Screen
+        name="Feed"
+        component={Details}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: MyIcon,
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={Order}
+        options={{
+          tabBarLabel: 'Updates',
+          tabBarIcon: MyIcon,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Settings}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: MyIcon,
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
